249b467280228af7d17cc58bf140a0ef64880fab
unknown <rockph0029@gmail.com>
2024-06-24 15:57:27 +0800

    add auth

---
+++ src/auth.ts
@@ -0,0 +1,8 @@
+import NextAuth from "next-auth"
+import GoogleProvider from 'next-auth/providers/google'
+ 
+export const { handlers, signIn, signOut, auth } = NextAuth({
+  providers: [
+        GoogleProvider
+  ],
+})
