019d9f3f4538784fe601f58b9dfa89c05ef0a556
unknown <you@example.com>
2024-07-01 21:17:54 +0800

    changes tonight

--- src/actions/tasks/add-task.ts
+++ src/actions/tasks/add-task.ts
@@ -12,17 +12,28 @@ export const addNewTask = actionClient
   .bindArgsSchemas<[userId: z.ZodString]>([z.string()])
   .action(async ({ parsedInput: Schema, bindArgsParsedInputs: [userId] }) => {
     try {
-      const res = await prisma.tasks.create({
-        data: {
-          taskTitle: Schema.taskTitle,
-          taskDescription: Schema.taskDescription,
-          icon: Schema.icon,
-          completionDate: new Date(Schema.completionDate),
-          iconBgColor: Schema.bgColor,
-          userId: userId,
-        },
-      });
-      revalidatePath("/tasks")
+
+    //     const user = await prisma.user.findFirst({
+    //         where: {
+    //             email: userId
+    //         }
+    //     })
+
+    //     if (!user) {
+    //         return [];
+    //     }
+
+    //   const res = await prisma.tasks.create({
+    //     data: {
+    //       taskTitle: Schema.taskTitle,
+    //       taskDescription: Schema.taskDescription,
+    //       icon: Schema.icon,
+    //       completionDate: new Date(Schema.completionDate),
+    //       iconBgColor: Schema.bgColor,
+    //       userId: user.id,
+    //     },
+    //   });
+      console.log(userId)
     } catch (error) {
       console.log({ message: error });
     }
