04621eb7d9feafec32a07f6a15598bd0756f2b12
unknown <you@example.com>
2024-07-15 20:54:30 +0800

    changes

--- src/sections/dashboard/upcoming-task.tsx
+++ src/sections/dashboard/upcoming-task.tsx
@@ -5,7 +5,7 @@ import {
   Card,
   CardTitle,
   CardDescription,
-  CardFooter
+  CardFooter,
 } from "@/components/ui/card";
 import { Separator } from "@/components/ui/separator";
 import { FlexBox } from "@/components/common/flex-box";
@@ -14,10 +14,10 @@ import { Progress } from "@/components/ui/progress";
 import { format, differenceInDays } from "date-fns";
 
 type UpcomingTaskProps = {
-    tasks: SampleType
-}
+  tasks: SampleType;
+};
 
-const UpcomingTask = ({tasks}: UpcomingTaskProps) => {
+const UpcomingTask = ({ tasks }: UpcomingTaskProps) => {
   return (
     <Card>
       <CardHeader>
@@ -27,34 +27,56 @@ const UpcomingTask = ({tasks}: UpcomingTaskProps) => {
       </CardHeader>
       <CardContent className="flex flex-col gap-4">
         {tasks.map((task) => (
-          <FlexBox
-            key={task.tasksId}
-            flexDirection="col"
-            className="rounded-lg py-6 px-4 bg-accent/65"
-          >
-            <FlexBox className="" alignItems="center">
-              <p className="font-extrabold text-[.90rem] text-foreground/75 tracking-tight leading-none basis-3/4 text-ellipsis overflow-hidden">
-                {task.taskTitle}
+          <Card>
+            {/* <CardContent className="flex flex-col">
+              <FlexBox className="" alignItems="center">
+                <p className="font-extrabold text-[.90rem] text-foreground/75 tracking-tight leading-none basis-3/4 text-ellipsis overflow-hidden">
+                  {task.taskTitle}
+                </p>
+                <p className="basis-1/4 text-[.750rem] font-semibold text-foreground/50 text-end">
+                  {format(task.completionDate, "EEE, MMM dd")}
+                </p>
+              </FlexBox>
+              <p className="text-sm text-foreground/50 font-medium tracking-tighter">
+                {task.taskDescription}
               </p>
-              <p className="basis-1/4 text-[.750rem] font-semibold text-foreground/50 text-end">
-                {format(task.completionDate, "EEE, MMM dd")}
-              </p>
-            </FlexBox>
-            <p className="text-sm text-foreground/50 font-medium tracking-tighter">
-              {task.taskDescription}
-            </p>
-            <FlexBox className="mt-2" justifyContent="between">
-              <p className="text-[.750rem] text-foreground/50 font-semibold tracking-tight text-end">
-                {`${task.progress} of 100%`}
-              </p>
-              <p className="text-[.750rem] text-foreground/50 font-semibold tracking-tight text-end">
-                {differenceInDays(task.completionDate, new Date()) +
-                  " " +
-                  "day left"}
-              </p>
-            </FlexBox>
-            <Progress className="h-[6px]" value={task.progress} />
-          </FlexBox>
+              <FlexBox className="mt-2" justifyContent="between">
+                <p className="text-[.750rem] text-foreground/50 font-semibold tracking-tight text-end">
+                  {`${task.progress} of 100%`}
+                </p>
+                <p className="text-[.750rem] text-foreground/50 font-semibold tracking-tight text-end">
+                  {differenceInDays(task.completionDate, new Date()) +
+                    " " +
+                    "day left"}
+                </p>
+                <Progress className="h-[6px]" value={task.progress} />
+              </FlexBox>
+            </CardContent> */}
+            <CardHeader className="flex flex-col justify-between items-center">
+              <FlexBox justifyContent="center">
+                <CardTitle className="font-extrabold text-[.90rem] flex items-center gap-1 text-foreground/75 tracking-tight leading-none basis-3/4 text-ellipsis overflow-hidden">
+                      <div className="h-10 w-10 rounded-md bg-blue-400"></div>
+                      <p>{task.taskTitle}</p>
+                </CardTitle>
+                <p className="basis-1/4 text-[.750rem] font-semibold text-foreground/65 text-end">
+                  {format(task.completionDate, "EEE, MMM dd")}
+                </p>
+              </FlexBox>
+            </CardHeader>
+            <CardContent className="">
+              <FlexBox justifyContent="between">
+                <p className="text-[.750rem] text-foreground/65 font-semibold tracking-tight text-end">
+                  {`${task.progress} of 100%`}
+                </p>
+                <p className="text-[.750rem] text-foreground/65 font-semibold tracking-tight text-end">
+                  {differenceInDays(task.completionDate, new Date()) +
+                    " " +
+                    "day/s left"}
+                </p>
+              </FlexBox>
+              <Progress className="h-[6px]" value={task.progress} />
+            </CardContent>
+          </Card>
         ))}
       </CardContent>
       <CardFooter>rex</CardFooter>
